fd1111c9090281b58f3f1a2c5c6decbc36de80bb
suxiaoshao <suxiaoshao@gamil.com>
2023-04-09 03:33:30 +0800

    feat(48): ok

---
+++ src/bin/48.rs
@@ -0,0 +1,25 @@
+struct Solution;
+
+impl Solution {
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        if matrix.len() <= 1 {
+            return;
+        }
+        let n = matrix.len();
+        for i in 0..(n / 2) {
+            for j in i..(n - i - 1) {
+                let tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    Solution::rotate(&mut matrix);
+    assert_eq!(matrix, vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3]]);
+}
